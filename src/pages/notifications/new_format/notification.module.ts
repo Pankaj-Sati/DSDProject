f14a52708a9c97ba66cf8ce380632e2fbb00be44
unknown <info.developerlabs@gmail.com>
2019-08-10 16:08:04 +0530

    Changed date formatting. Added manual padding in ion-content

--- src/pages/notifications/new_format/notification.module.ts
+++ src/pages/notifications/new_format/notification.module.ts
@@ -5,12 +5,15 @@ import { IonicPageModule, IonicPage } from 'ionic-angular';
 import { AddNotificationPage } from './add_notification/add_notification';
 import { NotificationListPage } from './notification_list/notification_list';
 
+//Pipes
+import { PipesModule } from '../../../pipes/pipes.module';
+
 //Components
 import {ComponentsModule } from '../../../components/components.module';
 
 @NgModule({
   declarations: [AddNotificationPage, NotificationListPage],
-  imports: [IonicPageModule.forChild(AddNotificationPage), ComponentsModule],
+  imports: [IonicPageModule.forChild(AddNotificationPage), ComponentsModule, PipesModule],
   entryComponents: [AddNotificationPage, NotificationListPage]
 })
 export class NotificationsModule
--- src/pages/reminders/new_format/reminders.module.ts
+++ src/pages/reminders/new_format/reminders.module.ts
@@ -5,12 +5,15 @@ import { IonicPageModule, IonicPage } from 'ionic-angular';
 import { AddReminderPage } from './add_reminder/add_reminder';
 import { ReminderListPage } from './reminder_list/reminder_list';
 
+//Pages
+import { PipesModule } from '../../../pipes/pipes.module';
+
 //Components
 import {ComponentsModule } from '../../../components/components.module';
 
 @NgModule({
   declarations: [AddReminderPage, ReminderListPage],
-  imports: [IonicPageModule.forChild(AddReminderPage), ComponentsModule],
+  imports: [IonicPageModule.forChild(AddReminderPage), ComponentsModule, PipesModule],
   entryComponents: [AddReminderPage, ReminderListPage]
 })
 export class RemindersModule
